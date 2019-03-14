a07e97f905066e4ca228ec477750910b65a9e06f
Anil Kumar H P <anilkumarhp@users.noreply.github.com>
2019-03-14 15:07:30 +0530

    Create collatzSequence.py
    A simple function to find collatz sequence for a given integer number.

---
+++ collatzSequence.py
@@ -0,0 +1,26 @@
+# function to find collatz sequence of a number.
+def collatz(number):
+    if number % 2 == 0:
+        eve = number // 2
+        print (eve)
+        return eve
+    else:
+        odd = 3 * number + 1;
+        print (odd)
+        return odd
+
+
+# Ask user to enter a integer value.
+while True:
+    try:
+        num = int(input("Enter an integer value: "))
+        break
+    except ValueError: # if entered value is not an integer.
+        print("Please only integer(number) value")
+
+
+# repeat until function returns 1
+while num != 1:
+    num = int(collatz(num))
+
+
